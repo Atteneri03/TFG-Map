5372d10e76f0dbdc5c917a345d08ab5ee625cebb
Atteneri Estévez <atteeste2003@gmail.com>
2024-03-05 16:27:34 +0000

    done

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,70 @@
 <script setup>
-
-import { ref , onMounted } from 'vue';
-import L from 'leaflet';
+import { onMounted, ref, toRaw } from "vue";
+import L, { marker } from "leaflet";
 
 const lat = ref(0);
 const lng = ref(0);
 const map = ref();
 const mapContainer = ref();
+var theMarker = {};
+var markers = [];
+var LeafIcon = L.Icon.extend({
+    options: {
+       iconSize:     [45, 45],
+       iconAnchor:   [25, 25],
+       popupAnchor:  [-3, -76]
+    }
+});
+
+var greenIcon = new LeafIcon({
+    iconUrl: 'green-pointer.webp'
+})
 
 onMounted(() => {
   getLocation();
- map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
-
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map.value);
 
-})
+  map.value.on("click", function(e){
+            new L.Marker([e.latlng.lat, e.latlng.lng] , {icon: greenIcon}).addTo(map.value);
+            markers.push(e.containerPoint);
+         });
+
+});
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.watchPosition((position) => {
       lat.value = position.coords.latitude;
       lng.value = position.coords.longitude;
       map.value.setView([lat.value, lng.value], 13);
 
-      L.marker([lat.value, lng.value],{draggable : false})
-      .addTo(map.value);
-      // .on("dragend",(event)=> {
-      //   lat.value = event.target.getLatLng().lat;
-      //   lng.value = event.target.getLatLng().lng;
-      // });
+      if (theMarker != undefined) {
+              map.value.removeLayer(theMarker);
+        };
+
+     theMarker = L.marker([lat.value, lng.value],{draggable : false})
+     .addTo(toRaw(map.value));
+      
+
 
     });
-  } 
+  }
 }
 
 </script>
 
 <template>
-<p>Latitude: {{ lat }}</p>
-<p>Longitude: {{ lng }}</p>
-<div ref="mapContainer" style="width: 400px; height: 400px;"></div>
+  
+<div>
+  <div ref="mapContainer" style="width: 400px; height: 400px"></div>
+</div>
+
 </template>
 
 <style scoped>
-
 </style>
